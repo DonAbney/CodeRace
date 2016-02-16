feff0c1afa02991ce7c9e763b46867c8cc826442
mhouse89 <matthew.house89@gmail.com>
2016-02-16 14:40:10 -0500

    Created a controller that will manage the actions associated with performing sign up. Added functionality to return the success page at the end of the action.

---
+++ controllers/doSignup.js
@@ -0,0 +1,16 @@
+function doSignup(req, res) {
+  var options = {
+    root: './views/',
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  };
+
+
+  res.sendFile('success.html', options);
+
+}
+
+module.exports = doSignup;
--- tests/controllers/signup_spec.js
+++ tests/controllers/signup_spec.js
@@ -1,4 +1,5 @@
 var getSignup = require('../../controllers/getSignup');
+var doSignup = require('../../controllers/doSignup')
 
 describe('Signup: ', function() {
 
@@ -16,4 +17,19 @@ describe('Signup: ', function() {
     });
 
   });
+
+  describe('DO', function() {
+    it('replies to request with the success page', function() {
+      var req, res, spy;
+      req = {};
+      res = {sendFile: function(){}};
+      spyOn(res, 'sendFile');
+
+      doSignup(req, res);
+
+      expect(res.sendFile).toHaveBeenCalledWith('success.html', jasmine.any(Object));
+    });
+
+  });
+
 });
