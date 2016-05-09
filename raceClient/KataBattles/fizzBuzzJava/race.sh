a77a45dee196eea9552d43018f7577ca89829f28
Matt House <matthew.house89@gmail.com>
2016-05-09 14:50:59 -0400

    updated script in the Java projects to run using the same command + env variable as Javascript.

--- raceClient/KataBattles/fizzBuzzJava/race.sh
+++ raceClient/KataBattles/fizzBuzzJava/race.sh
@@ -1,6 +1,6 @@
 while true;
   do
     clear;
-    gradle clean accept report --continue --rerun-tasks -Pracer=$1;
+    gradle clean accept report --continue --rerun-tasks -Pracer=$RACER_NAME;
     sleep 5;
   done;
